26bbf085095eb013465f6b6476ac12452d503175
Zach Stein <zstein@holts.com>
2020-06-07 16:09:25 -0400

    Mark up component in external template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Template in component demo</h1>
-    <p> Blah blah blah</p>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
