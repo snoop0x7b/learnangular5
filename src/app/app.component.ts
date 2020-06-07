39328b9ddc24bbe1c5e15161b95e55611d7ed382
Zach Stein <zstein@holts.com>
2020-06-07 16:31:12 -0400

    Initial use of a variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'my app';
+  query: string;
+
+  constructor() {
+    this.query = 'Barot';
+  }
 }
