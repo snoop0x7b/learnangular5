b78e56f78123becdd40fc5924bfe2fba9dad440f
Zach Stein <zstein@holts.com>
2020-06-08 19:13:47 -0400

    Set up pipe for search engine

--- src/app/artist-items/artist-items.component.ts
+++ src/app/artist-items/artist-items.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-artist-items',
   templateUrl: './artist-items.component.html',
-  inputs: ['artist']
 })
 export class ArtistItemsComponent implements OnInit {
 
+  @Input()  artist: object;
   constructor() { }
 
   ngOnInit() {
